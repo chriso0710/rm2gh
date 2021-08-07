2cb2b09198bfe3b57a319f224885ef379860f0d9
Christian Ott <mail@christian.ott.name>
2021-08-07 16:24:00 +0200

    refactored, octokit

--- export.rb
+++ export.rb
@@ -3,9 +3,9 @@ require 'excon'
 require 'json'
 require 'awesome_print'
 require 'ostruct'
+require 'octokit'
 
-class Redmine
-
+class RedmineExporter
     ISSUES_PATH = "/issues.json"
     ISSUE_PATH = "/issues/%s.json"
     PROJECTS_PATH = "/projects.json"
@@ -16,9 +16,11 @@ class Redmine
         Dir.mkdir(ENV['JSON_DIR']) unless Dir.exist?(ENV['JSON_DIR'])
     end
 
-    def connection_test
-        puts "Connecting to #{ENV['REDMINE_URL']}"
+    def stats
         puts "Number of projects: #{pages(PROJECTS_PATH).total}"
+        projects.each do |p|
+            puts " ##{p.id} - #{p.name}"
+        end
     end
 
     def pages(path, options = {})
@@ -28,7 +30,7 @@ class Redmine
         j = JSON.parse response.body
         total = j["total_count"]
         pages = (total/100.0).ceil
-        OpenStruct.new(:total => total, :pages => pages)
+        OpenStruct.new(:path => path, :total => total, :pages => pages)
     end
 
     def projects
@@ -44,12 +46,11 @@ class Redmine
 
     def issues
         issues = []
-        options = {}
-        options.merge!({:project_id => ENV['REDMINE_PROJECT_ID'].to_i}) if ENV['REDMINE_PROJECT_ID']
-        options.merge!({:status_id => "*"})
-        pages(ISSUES_PATH, options).pages.times do |p|
-            query = {:limit => 100, :offset => p*100}
-            query.merge!(options)
+        query = {}
+        query.merge!({:project_id => ENV['REDMINE_PROJECT_ID'].to_i}) if ENV['REDMINE_PROJECT_ID']
+        query.merge!({:status_id => "*"})
+        pages(ISSUES_PATH, query).pages.times do |p|
+            query.merge!({:limit => 100, :offset => p*100})
             response = @connection.get(:path => ISSUES_PATH, :query => query, :expects => [200, 201])
             j = JSON.parse(response.body)
             issues.push(*j["issues"])
@@ -70,24 +71,38 @@ class Redmine
     def create_files
         issues.each do |i|
             id = i["id"]
-            puts "id: #{id} - #{i["project"]["name"]} - #{i["subject"]}"
-            File.open(filename(id),"w") do |f|
-                f.write(JSON.pretty_generate(issue(id)))
-            end
+            puts "id: #{id} - #{i["project"]["name"]} - #{i["subject"]} - #{i["status"]["name"]}"
+            File.write(filename(id), JSON.pretty_generate(issue(id))) unless File.exist?(filename(id))
         end    
+    end
+
+end
+
+class GithubImporter
+
+    def initialize
+        @client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
+        @client.auto_paginate = true
+    end
+
+    def stats
+        puts "Logged into Github as #{@client.user.login}, #{@client.user.name}"
+        puts "Number of repos: #{@client.repos.size}"
+        @client.repos.each do |r|
+            puts " ##{r.id} - #{r.full_name}"
+        end
     end
 
 end
 
 Dotenv.load
 
-redmine = Redmine.new
-redmine.connection_test
-projects = redmine.projects
-projects.each do |p|
-    puts "id: #{p.id} - #{p.name}"
-end
-issues = redmine.issues
-puts "Number of issues: #{issues.size}"
-ap issues.first.to_json
-redmine.create_files
+redmine = RedmineExporter.new
+# puts "Connecting to #{ENV['REDMINE_URL']}"
+# redmine.stats
+# issues = redmine.issues
+# puts "Number of issues: #{issues.size}"
+# redmine.create_files
+
+github = GithubImporter.new
+github.stats
