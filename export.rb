03a0d01ddefd0b2981bd4d05818b13f4b2d87c91
Christian Ott <mail@christian.ott.name>
2021-08-09 15:01:56 +0200

    github api test

--- export.rb
+++ export.rb
@@ -16,13 +16,6 @@ class RedmineExporter
         Dir.mkdir(ENV['JSON_DIR']) unless Dir.exist?(ENV['JSON_DIR'])
     end
 
-    def stats
-        puts "Number of projects: #{pages(PROJECTS_PATH).total}"
-        projects.each do |p|
-            puts " ##{p.id} - #{p.name}"
-        end
-    end
-
     def pages(path, options = {})
         query = {:limit => 1}
         query.merge!(options)
@@ -33,18 +26,22 @@ class RedmineExporter
         OpenStruct.new(:path => path, :total => total, :pages => pages)
     end
 
-    def projects
+    def load_projects
         projects = []
         pages(PROJECTS_PATH).pages.times do |p|
             query = {:limit => 100, :offset => p*100}
             response = @connection.get(:path => PROJECTS_PATH, :query => query, :expects => [200, 201])
-            j = JSON.parse(response.body, object_class: OpenStruct)
+            j = JSON.parse(response.body)
             projects.push(*j["projects"])
         end
         projects
     end
 
-    def issues
+    def projects
+        @projects ||= load_projects
+    end
+
+    def load_issues
         issues = []
         query = {}
         query.merge!({:project_id => ENV['REDMINE_PROJECT_ID'].to_i}) if ENV['REDMINE_PROJECT_ID']
@@ -58,6 +55,10 @@ class RedmineExporter
         issues
     end
 
+    def issues
+        @issues ||= load_issues
+    end
+
     def issue(id)
         query = {:include => "journals"}
         response = @connection.get(:path => ISSUE_PATH % id, :query => query, :expects => [200, 201])
@@ -68,12 +69,31 @@ class RedmineExporter
         "#{ENV['JSON_DIR']}/#{id}.json"
     end
 
-    def create_files
+    def export
         issues.each do |i|
             id = i["id"]
-            puts "id: #{id} - #{i["project"]["name"]} - #{i["subject"]} - #{i["status"]["name"]}"
             File.write(filename(id), JSON.pretty_generate(issue(id))) unless File.exist?(filename(id))
-        end    
+        end
+    end
+
+    def priority_labels
+        issues.map{ |i| i["priority"]["name"] if i["priority"] }.uniq.compact
+    end
+
+    def tracker_labels
+        issues.map{ |i| i["tracker"]["name"] if i["tracker"] }.uniq.compact
+    end
+
+    def status_labels
+        issues.map{ |i| i["status"]["name"] if i["status"] }.uniq.compact
+    end
+
+    def assigned_to
+        issues.map{ |i| i["assigned_to"]["name"] if i["assigned_to"] }.uniq.compact
+    end
+
+    def category_labels
+        issues.map{ |i| i["category"]["name"] if i["category"] }.uniq.compact
     end
 
 end
@@ -88,8 +108,23 @@ class GithubImporter
     def stats
         puts "Logged into Github as #{@client.user.login}, #{@client.user.name}"
         puts "Number of repos: #{@client.repos.size}"
-        @client.repos.each do |r|
-            puts " ##{r.id} - #{r.full_name}"
+        @client.repos.each { |r| puts " ##{r.id} - #{r.full_name}" }
+        ap @client.labels(ENV['GITHUB_REPO'])
+        ap @client.list_milestones(ENV['GITHUB_REPO'])
+        ap @client.repository_assignees(ENV['GITHUB_REPO'])
+        # response = @client.create_issue(ENV['GITHUB_REPO'], 'Updated Docs', 'Added some extra links')
+        # ap response
+    end
+
+    def meta(issue)
+        i = issue.issue
+        puts "#{i.id} - #{i.tracker.name}"
+    end
+
+    def import
+        Dir.entries(ENV['JSON_DIR']).reject {|f| File.directory? f}.each do |f| 
+            issue = File.read("#{ENV['JSON_DIR']}/#{f}")
+            meta JSON.parse(issue, object_class: OpenStruct)
         end
     end
 
@@ -98,11 +133,25 @@ end
 Dotenv.load
 
 redmine = RedmineExporter.new
-# puts "Connecting to #{ENV['REDMINE_URL']}"
-# redmine.stats
-# issues = redmine.issues
-# puts "Number of issues: #{issues.size}"
-# redmine.create_files
+puts "Connecting to #{ENV['REDMINE_URL']}"
+puts "Number of projects: #{redmine.projects.size}"
+redmine.projects.each { |p| puts " ##{p["id"]} - #{p["name"]}" }
+puts "Number of issues to export: #{redmine.issues.size}"
+puts "Unique Priorities: #{redmine.priority_labels.join(",")}"
+puts "Unique Categories: #{redmine.category_labels.join(",")}"
+puts "Unique Trackers: #{redmine.tracker_labels.join(",")}"
+puts "Unique Statuses: #{redmine.status_labels.join(",")}"
+puts "Unique Assignees: #{redmine.assigned_to.join(",")}"
+
+redmine.issues.each do |i|
+   id = i["id"]
+   # puts "id: #{id} - #{i["project"]["name"]} - #{i["subject"]} - #{i["status"]["name"]}"
+end
+# redmine.export
+ap eval ENV['TRACKER_MAP']
+ap eval ENV['CATEGORY_MAP']
+ap eval ENV['STATUS_OPENCLOSE_MAP']
 
 github = GithubImporter.new
-github.stats
+github.import
+# github.stats
